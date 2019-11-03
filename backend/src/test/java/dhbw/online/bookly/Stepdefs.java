8942841dde984a5349c2ae58993df0c59dc76cc3
Jeanne Helm <jeanne.helm@diva-e.com>
2019-11-03 11:23:56 +0100

    copy generated code by cucumber into its step defs

--- backend/src/test/java/dhbw/online/bookly/Stepdefs.java
+++ backend/src/test/java/dhbw/online/bookly/Stepdefs.java
@@ -7,5 +7,88 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+    @Given("I Am logged in")
+    public void i_Am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I navigate to Profile")
+    public void i_navigate_to_Profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("User Pages are loaded")
+    public void user_Pages_are_loaded() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("User Pages are shown")
+    public void user_Pages_are_shown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select delete page")
+    public void i_select_delete_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The page should be refreshed")
+    public void the_page_should_be_refreshed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I am not able to see the deleted page")
+    public void i_am_not_able_to_see_the_deleted_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I am logged in")
+    public void i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select Friendship Book")
+    public void i_select_Friendship_Book() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can browse through the pages")
+    public void i_can_browse_through_the_pages() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select add entry")
+    public void i_select_add_entry() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can copy the generated link and share it with someone")
+    public void i_can_copy_the_generated_link_and_share_it_with_someone() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select edit page")
+    public void i_select_edit_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can edit the page")
+    public void i_can_edit_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
 }
