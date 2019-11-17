c66df46199cc4a3147b367ba4fe6a61923be03e5
Jeanne Helm <jeanne.helm@diva-e.com>
2019-11-17 10:59:34 +0100

    add user:password authentication

---
+++ backend/src/main/java/dhbw/online/bookly/BookRepository.java
@@ -0,0 +1,6 @@
+package dhbw.online.bookly;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
---
+++ backend/src/main/java/dhbw/online/bookly/error/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package dhbw.online.bookly.error;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
