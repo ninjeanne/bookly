f854f0b52d89c143c8764b146be2013972c56bad
Jeanne Helm <jeanne.helm@diva-e.com>
2019-11-22 18:56:56 +0100

    outsource the page service methods into their own class and refactor them

--- backend/src/main/java/dhbw/online/bookly/controller/PageController.java
+++ backend/src/main/java/dhbw/online/bookly/controller/PageController.java
@@ -1,12 +1,12 @@
 package dhbw.online.bookly.controller;
 
-import dhbw.online.bookly.dto.FriendshipBook;
 import dhbw.online.bookly.dto.Page;
 import dhbw.online.bookly.dto.User;
 import dhbw.online.bookly.exception.FriendshipBookException;
 import dhbw.online.bookly.exception.PageException;
-import dhbw.online.bookly.service.FriendshipBookService;
+import dhbw.online.bookly.service.PageService;
 import dhbw.online.bookly.service.UserService;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class PageController {
 
     @Autowired
-    private FriendshipBookService bookService;
+    private PageService pageService;
 
     @Autowired
     private UserService userService;
@@ -27,8 +27,8 @@ public class PageController {
         User user = userService.getUser();
         if (user != null && page != null) {
             try {
-                FriendshipBook book = bookService.addPage(user, page);
-                return ResponseEntity.ok(book.getPages());
+                val pages = pageService.add(user, page);
+                return ResponseEntity.ok(pages);
             } catch (FriendshipBookException | PageException fbe) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body(fbe.getMessage());
             }
@@ -40,9 +40,9 @@ public class PageController {
     ResponseEntity read() {
         User user = userService.getUser();
         if (user != null) {
-            FriendshipBook book = bookService.read(user);
-            if (book != null)
-                return ResponseEntity.ok(book.getPages());
+            val pages = pageService.read(user);
+            if (pages != null)
+                return ResponseEntity.ok(pages);
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
@@ -53,8 +53,8 @@ public class PageController {
         User user = userService.getUser();
         if (user != null) {
             try {
-                FriendshipBook book = bookService.updatePage(user, page);
-                return ResponseEntity.ok(book.getPages());
+                val pages = pageService.update(user, page);
+                return ResponseEntity.ok(pages);
             } catch (FriendshipBookException | PageException fbe) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body(fbe.getMessage());
             }
@@ -67,8 +67,8 @@ public class PageController {
         User user = userService.getUser();
         if (user != null && page != null) {
             try {
-                FriendshipBook book = bookService.deletePage(user, page);
-                return ResponseEntity.ok(book.getPages());
+                val pages = pageService.delete(user, page);
+                return ResponseEntity.ok(pages);
             } catch (FriendshipBookException | PageException fbe) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body(fbe.getMessage());
             }
--- backend/src/main/java/dhbw/online/bookly/service/FriendshipBookService.java
+++ backend/src/main/java/dhbw/online/bookly/service/FriendshipBookService.java
@@ -1,14 +1,10 @@
 package dhbw.online.bookly.service;
 
 import dhbw.online.bookly.dto.FriendshipBook;
-import dhbw.online.bookly.dto.Page;
 import dhbw.online.bookly.dto.User;
 import dhbw.online.bookly.exception.FriendshipBookException;
-import dhbw.online.bookly.exception.PageException;
 import dhbw.online.bookly.repository.FriendshipBookRepository;
-import dhbw.online.bookly.repository.PageRepository;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
@@ -16,15 +12,12 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class FriendshipBookService {
     @Autowired
     private FriendshipBookRepository repository;
-    @Autowired
-    private PageRepository pageRepository;
 
     public boolean create(User user) {
         if (!repository.existsByUser(user)) {
@@ -61,61 +54,5 @@ public class FriendshipBookService {
             return repository.save(book);
         }
         throw new FriendshipBookException("There is no book for user " + user.getUsername());
-    }
-
-    public FriendshipBook addPage(User user, Page page) {
-        FriendshipBook book = read(user);
-        if (book != null) {
-            val pages = book.getPages();
-            page.setUuid(UUID.randomUUID().toString());
-            setMissingUuidsToPageEntries(page);
-            pages.add(page);
-            return repository.save(book);
-        }
-        throw new FriendshipBookException("There is no book for user " + user.getUsername());
-    }
-
-    private Page getPage(String uuid) {
-        return pageRepository.findByUuid(uuid).orElse(null);
-    }
-
-    public FriendshipBook deletePage(User user, Page page) {
-        FriendshipBook book = read(user);
-        if (book != null) {
-            val pagesFromDb = book.getPages();
-            if (pageRepository.existsByUuid(page.getUuid())) {
-                val pageFromDb = getPage(page.getUuid());
-                pagesFromDb.remove(pageFromDb);
-                return repository.save(book);
-            }
-        }
-        throw new FriendshipBookException("There is no book for user " + user.getUsername());
-    }
-
-    public FriendshipBook updatePage(User user, Page page) {
-        FriendshipBook book = read(user);
-        if (book != null) {
-            val pages = book.getPages();
-            val resultpages = pages.stream()
-                    .filter(pageFromDb -> pageFromDb.getUuid().equals(page.getUuid()))
-                    .collect(Collectors.toList());
-            if (resultpages.isEmpty()) {
-                throw new PageException("Requested page with the id " + page.getUuid() + " does not exist! ");
-            }
-            setMissingUuidsToPageEntries(page);
-            pages.remove(resultpages.get(0));
-            pages.add(page);
-            return repository.save(book);
-        }
-        throw new FriendshipBookException("There is no book for user " + user.getUsername());
-    }
-
-    private void setMissingUuidsToPageEntries(Page page) {
-        if (page.getEntries().isEmpty()) {
-            page.setEntries(new ArrayList<>());
-        }
-        page.getEntries().stream()
-                .filter(pageEntry -> pageEntry.getUuid() == null)
-                .forEach(pageEntry -> pageEntry.setUuid(UUID.randomUUID().toString()));
     }
 }
