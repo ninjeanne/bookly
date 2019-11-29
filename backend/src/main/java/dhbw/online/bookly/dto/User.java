de0951b1083b07786056ee6159103ea143c803cb
Jeanne Helm <jeanne.helm@diva-e.com>
2019-11-29 14:09:08 +0100

    implement a first test

--- backend/src/main/java/dhbw/online/bookly/dto/User.java
+++ backend/src/main/java/dhbw/online/bookly/dto/User.java
@@ -1,14 +1,20 @@
 package dhbw.online.bookly.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
@@ -16,7 +22,7 @@ import javax.persistence.Id;
 @Builder
 @Entity
 @ApiModel(description = "Class representing a user tracked by the application.")
-public class User {
+public class User implements UserDetails {
     @Id
     @ApiModelProperty(notes = "The username is extracted by the authentication\n" +
             "Unique identifier of the user to log in. No two users can have the same username. " +
@@ -40,4 +46,32 @@ public class User {
             example = "mustermann",
             position = 3)
     private String last_name;
+
+    @JsonIgnore
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- backend/src/test/java/dhbw/online/bookly/Stepdefs.java
+++ backend/src/test/java/dhbw/online/bookly/Stepdefs.java
@@ -3,8 +3,18 @@ package dhbw.online.bookly;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import dhbw.online.bookly.configuration.SpringSecurityWebAuxTestConfig;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = SpringSecurityWebAuxTestConfig.class
+)
+@AutoConfigureMockMvc
 public class Stepdefs {
 
     @Given("^I am logged in$")
