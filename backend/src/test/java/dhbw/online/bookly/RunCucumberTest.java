9bb2c5fc34ee45e50f9e2b9994cee6bbfdb0acda
Jeanne Helm <jeanne.helm@diva-e.com>
2019-11-29 17:56:35 +0100

    make testing with cucumber great again :D

--- backend/src/test/java/dhbw/online/bookly/RunCucumberTest.java
+++ backend/src/test/java/dhbw/online/bookly/RunCucumberTest.java
@@ -5,8 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
-        features = "src/test/resources/dhbw/online/bookly"
+@CucumberOptions(
+        glue = {"cucumber.api.spring", "dhbw.online.bookly"},
+        features = {"src/test/resources"}
 )
 public class RunCucumberTest {
 }
