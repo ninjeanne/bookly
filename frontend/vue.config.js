60fe6d8f1997dd874811dd2960aba79ea2d3277c
Jeanne Helm <jeanne.helm@diva-e.com>
2020-04-20 09:16:17 +0200

    try out cors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,10 +6,10 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+        target: 'https:/bookly.online', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
         ws: true,
         changeOrigin: true
-      }
+      },
     }
   },
   // Change build paths to make them Maven compatible
