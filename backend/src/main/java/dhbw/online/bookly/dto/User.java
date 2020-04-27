0b56f76caf09a1539086c6a377ed21c6a96dbb6d
Jeanne Helm <jeanne.helm@diva-e.com>
2020-04-27 17:12:02 +0200

    rename table user to registeredbooklyuser

--- backend/src/main/java/dhbw/online/bookly/dto/User.java
+++ backend/src/main/java/dhbw/online/bookly/dto/User.java
@@ -1,6 +1,5 @@
 package dhbw.online.bookly.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -10,8 +9,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Arrays;
-import java.util.Collection;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
@@ -19,29 +17,22 @@ import java.util.Collection;
 @Builder
 @Entity
 @ApiModel(description = "Class representing a user tracked by the application.")
+@Table(name = "RegisteredBooklyUser")
 public class User {
     @Id
-    @ApiModelProperty(notes = "The username is extracted by the authentication\n" +
-            "Unique identifier of the user to log in. No two users can have the same username. " +
-            "The username can't be changed once an account has been created. " +
-            "The username is extracted by the authentication",
-            example = "maxmustermann",
-            position = 0)
+    @ApiModelProperty(
+            notes = "The username is extracted by the authentication\n" + "Unique identifier of the user to log in. No two users can have the same username. "
+                    + "The username can't be changed once an account has been created. " + "The username is extracted by the authentication",
+            example = "maxmustermann", position = 0)
     private String username;
 
-    @ApiModelProperty(notes = "mail address of the user.",
-            example = "max@bookly.online",
-            position = 1)
+    @ApiModelProperty(notes = "mail address of the user.", example = "max@bookly.online", position = 1)
     private String mail;
 
-    @ApiModelProperty(notes = "first name of the user",
-            example = "max",
-            position = 2)
+    @ApiModelProperty(notes = "first name of the user", example = "max", position = 2)
     private String first_name;
 
-    @ApiModelProperty(notes = "last name of the user.",
-            example = "mustermann",
-            position = 3)
+    @ApiModelProperty(notes = "last name of the user.", example = "mustermann", position = 3)
     private String last_name;
 
 }
