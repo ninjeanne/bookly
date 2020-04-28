725c5f5087f2570cd1dbb112f279322ff4dea61e
Jeanne Helm <jeanne.helm@diva-e.com>
2020-04-28 12:59:00 +0200

    try out module export without dev

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,26 +1,5 @@
 // vue.config.js
-let baseURL;
-if(location.hostname === "localhost"){
-  baseURL = "http://localhost:8080/api"
-}
-else {
-  baseURL = "https://" + location.hostname + "/api"
-}
-
 module.exports = {
-  // proxy all webpack dev-server requests starting with /api
-  // to our Spring Boot backend (localhost:8080) using http-proxy-middleware
-  // see https://cli.vuejs.org/config/#devserver-proxy
-  devServer: {
-    proxy: {
-      '/api': {
-        target: baseURL, // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
-        ws: true,
-        changeOrigin: true
-      },
-    }
-  },
-  // Change build paths to make them Maven compatible
   // see https://cli.vuejs.org/config/
   outputDir: 'target/dist',
   assetsDir: 'static'
