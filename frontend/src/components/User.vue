6d8906f20e0f47930de41bfdefaf062ec5e2a4b8
rehcan <39638060+rehcan@users.noreply.github.com>
2020-05-01 17:25:01 +0200

    - finally working PageEditor - changed Page layout - complete invite functionality (generate code => put in code => edit page) - still some bugs (user logs out after using public/api) - can't preview already uploaded images

--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -2,6 +2,8 @@
   <div>
     <h2>Hey {{ username }}!!</h2>
     <button v-on:click="logout">Logout</button>
+    <button v-on:click="invite">Invite Friend</button>
+    <h2>Code: {{ invite_code }} </h2>
   </div>
 </template>
 
@@ -12,18 +14,24 @@
     name: 'user',
 
     beforeMount() {
-      console.log(localStorage.getItem("userInfo"));
       this.username = JSON.parse(localStorage.getItem('userInfo')).preferred_username;
     },
     data() {
       return {
-        username: ""
+        username: "",
+        invite_code: ""
       }
     },
     methods: {
       logout: function () {
         this.$router.push("/");
         keycloak.logout();
+      },
+      invite: function () {
+        this.$store.dispatch("newPage")
+                .then((response) => {
+                  this.invite_code = response.data.uuid;
+                })
       }
     }
   }
