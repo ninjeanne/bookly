fb9cfe46b661606ab7d705a301eb9287aa826843
admin <administrator@mainstorconcept.de>
2020-05-11 20:22:02 +0200

    generate page UUID from RandomWordList

--- backend/src/main/java/dhbw/online/bookly/dto/Page.java
+++ backend/src/main/java/dhbw/online/bookly/dto/Page.java
@@ -11,81 +11,114 @@ import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.NoSuchFileException;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 @Data
 @Entity
 @Builder
 @ApiModel(description = "page for a visitor")
 public class Page {
-    public Page(String uuid, PageImage pageImage, String name,String address, String telephone,
-                String mobile,String school_class, String school,  String size,
-                String hair_color, String eye_color,String birthday,String star_sign,
-                String favorite_subject,String favorite_pet,  String how_to_please_me,
-                String what_i_dont_like,String favorite_job,String my_hobbies,
-                String fan_of,String favorite_movie,String favorite_sport, String favorite_book,
-                String favorite_food, String nice_comment, String date, String leftOver)
-    {   if(uuid.equals("")){this.uuid=generateRandomUUID();}else this.uuid=uuid;
-        this.pageImage=pageImage;
-        this.name=name;
-        this.address=address;
-        this.telephone=telephone;
-        this.mobile=mobile;
-        this.school_class=school_class;
-        this.school=school;
-        this.size=size;
-        this.hair_color=hair_color;
-        this.eye_color=eye_color;
-        this.birthday=birthday;
-        this.star_sign=star_sign;
-        this.favorite_subject=favorite_subject;
-        this.favorite_pet=favorite_pet;
-        this.how_to_please_me=how_to_please_me;
-        this.what_i_dont_like=what_i_dont_like;
-        this.favorite_job=favorite_job;
-        this.my_hobbies=my_hobbies;
-        this.fan_of=fan_of;
-        this.favorite_movie=favorite_movie;
-        this.favorite_sport=favorite_sport;
-        this.favorite_book=favorite_book;
-        this.favorite_food=favorite_food;
-        this.nice_comment=nice_comment;
-        this.date=date;
-        this.leftOver=leftOver;
+    public Page(String uuid, PageImage pageImage, String name, String address, String telephone,
+                String mobile, String school_class, String school, String size,
+                String hair_color, String eye_color, String birthday, String star_sign,
+                String favorite_subject, String favorite_pet, String how_to_please_me,
+                String what_i_dont_like, String favorite_job, String my_hobbies,
+                String fan_of, String favorite_movie, String favorite_sport, String favorite_book,
+                String favorite_food, String nice_comment, String date, String leftOver) {
+        if (uuid == null || uuid.equals("")) {
+            this.uuid = generateRandomUUIDfromList();
+        } else this.uuid = uuid;
+        this.pageImage = pageImage;
+        this.name = name;
+        this.address = address;
+        this.telephone = telephone;
+        this.mobile = mobile;
+        this.school_class = school_class;
+        this.school = school;
+        this.size = size;
+        this.hair_color = hair_color;
+        this.eye_color = eye_color;
+        this.birthday = birthday;
+        this.star_sign = star_sign;
+        this.favorite_subject = favorite_subject;
+        this.favorite_pet = favorite_pet;
+        this.how_to_please_me = how_to_please_me;
+        this.what_i_dont_like = what_i_dont_like;
+        this.favorite_job = favorite_job;
+        this.my_hobbies = my_hobbies;
+        this.fan_of = fan_of;
+        this.favorite_movie = favorite_movie;
+        this.favorite_sport = favorite_sport;
+        this.favorite_book = favorite_book;
+        this.favorite_food = favorite_food;
+        this.nice_comment = nice_comment;
+        this.date = date;
+        this.leftOver = leftOver;
     }
 
-    public Page(){
-        this.uuid=generateRandomUUID();
-        this.pageImage=new PageImage();
-        this.name="";
-        this.address="";
-        this.telephone="";
-        this.mobile="";
-        this.school_class="";
-        this.school="";
-        this.size="";
-        this.hair_color="";
-        this.eye_color="";
-        this.birthday="";
-        this.star_sign="";
-        this.favorite_subject="";
-        this.favorite_pet="";
-        this.how_to_please_me="";
-        this.what_i_dont_like="";
-        this.favorite_job="";
-        this.my_hobbies="";
-        this.fan_of="";
-        this.favorite_movie="";
-        this.favorite_sport="";
-        this.favorite_book="";
-        this.favorite_food="";
-        this.nice_comment="";
-        this.date="";
-        this.leftOver="";
+    public Page() {
+        this.uuid = generateRandomUUIDfromList();
+        this.pageImage = new PageImage();
+        this.name = null;
+        this.address = null;
+        this.telephone = null;
+        this.mobile = null;
+        this.school_class = null;
+        this.school = null;
+        this.size = null;
+        this.hair_color = null;
+        this.eye_color = null;
+        this.birthday = null;
+        this.star_sign = null;
+        this.favorite_subject = null;
+        this.favorite_pet = null;
+        this.how_to_please_me = null;
+        this.what_i_dont_like = null;
+        this.favorite_job = null;
+        this.my_hobbies = null;
+        this.fan_of = null;
+        this.favorite_movie = null;
+        this.favorite_sport = null;
+        this.favorite_book = null;
+        this.favorite_food = null;
+        this.nice_comment = null;
+        this.date = null;
+        this.leftOver = null;
+    }
+
+    public Page(String pageUUID) {
+        this.uuid = pageUUID;
+        this.pageImage = new PageImage();
+        this.name = null;
+        this.address = null;
+        this.telephone = null;
+        this.mobile = null;
+        this.school_class = null;
+        this.school = null;
+        this.size = null;
+        this.hair_color = null;
+        this.eye_color = null;
+        this.birthday = null;
+        this.star_sign = null;
+        this.favorite_subject = null;
+        this.favorite_pet = null;
+        this.how_to_please_me = null;
+        this.what_i_dont_like = null;
+        this.favorite_job = null;
+        this.my_hobbies = null;
+        this.fan_of = null;
+        this.favorite_movie = null;
+        this.favorite_sport = null;
+        this.favorite_book = null;
+        this.favorite_food = null;
+        this.nice_comment = null;
+        this.date = null;
+        this.leftOver = null;
     }
 
     @Id
@@ -175,8 +208,33 @@ public class Page {
     @ApiModelProperty(notes = "last words of the friend", position = 26)
     private String leftOver;
 
+    private String generateRandomUUIDfromList() {
+        final long numberOfLines = 370100L;
+        long randomLineNumber1 = getRandomNumberLong(1, numberOfLines);
+        long randomLineNumber2 = getRandomNumberLong(1, numberOfLines);
+        long randomLineNumber3 = getRandomNumberLong(1, numberOfLines);
+        StringBuilder newPageUUID=new StringBuilder();
+        try {
+            try (FileReader readfile = new FileReader("./backend/src/main/resources/words_alpha.txt")) {
+                BufferedReader readbuffer = new BufferedReader(readfile);
+                for (long lineNumber = 1; lineNumber < numberOfLines; lineNumber++) {
+                    if (lineNumber == randomLineNumber1 ||
+                            lineNumber == randomLineNumber2 ||
+                            lineNumber == randomLineNumber3) {
+                        newPageUUID.append(readbuffer.readLine()+":");
+                    } else
+                        readbuffer.readLine();
+                }
+                readbuffer.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newPageUUID.toString();
+    }
+
     private String generateRandomUUID() {
-        String UUID = "no:UUID:Set";
+        String pageUUID = "no:pageUUID:Set";
         try {
             URL url = new URL("https://random-word-api.herokuapp.com/word?number=3");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -193,16 +251,20 @@ public class Page {
                 inputLine = inputLine.replaceAll("\"]", "");
                 content.append(inputLine);
             }
-            UUID = content.toString();
+            pageUUID = content.toString();
             in.close();
             con.disconnect();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(UUID.equals("no:UUID:Set")){
+        if (pageUUID.equals("no:UUID:Set")) {
             throw new PageException("No UUID Set");
         }
-        return UUID;
+        return pageUUID;
     }
 
+    public static long getRandomNumberLong(long min, long max) {
+        Random random = new Random();
+        return random.longs(min, (max + 1)).findFirst().getAsLong();
+    }
 }
