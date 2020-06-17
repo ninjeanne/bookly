3da371317c6188a39bb9212fc3edd5b4ee906163
admin <administrator@mainstorconcept.de>
2020-06-17 22:31:00 +0200

    help

--- frontend/src/components/Help.vue
+++ frontend/src/components/Help.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <h2>Help</h2>
+        <img src="../assets/help_invite_friend.JPG" alt="Trulli" width="500" height="333">
+
+        <p>This page will give you a idea what bookly can do.</p>
+
+        <h3>Login</h3>
+
+        <h3>Edit profile</h3>
+        <h3>Edit cover</h3>
+        <h3>Invite friend to friendship book</h3>
+        <h3>Create new friendship book page</h3>
     </div>
 </template>
 
