3813aa143eb05e9f2a6a7363cc674d555437f708
admin <administrator@mainstorconcept.de>
2020-06-18 00:52:31 +0200

    help

--- frontend/src/components/Help.vue
+++ frontend/src/components/Help.vue
@@ -1,16 +1,46 @@
 <template>
-    <div>
-        <h2>Help</h2>
-        <img src="../assets/help_invite_friend.JPG" alt="Trulli" width="500" height="333">
+    <div class="main" style="text-align:left">
+        <h1>Help</h1>
 
-        <p>This page will give you a idea what bookly can do.</p>
+        <p>This page will give you an idea of what bookly can do. Here you can find a quick overview of our help topics. You can choose the topic that interests you.</p>
+        <table>
+            <li> <a href="#login">Log in to your account</a></li>
+            <li> <a href="#inviteFriend">Invite friend to friendship book</a></li>
+            <li> <a href="#register">Register for an account at bookly</a></li>
+            <li> <a href="https://www.youtube.com/watch?v=6JA0YSH2aXc">Bookly help video</a></li>
+        </table>
+        <br>
 
-        <h3>Login</h3>
+        <h4><a id="Login">Log in to your account</a></h4>
+        <br>
+        <p> In order to log in to your account, click on "Book" in the header or on "Go to YOUR book" under the welcome message on the home website. Fill in your login data. Press login to see your friendship book. Once you are logged in you can edit your profile and invite friends to design a friendship book page for you.</p>
+        <div style="text-align:center"> <img src="../assets/loginBookly.jpg" alt="Tax" width="300" height="250" >
+        </div>
+        <br>
+        <h4><a id="inviteFriend">Invite friend to friendship book</a></h4>
+        <br>
+        <img src="../assets/help_invite_friend_2.jpg" alt="Tax" width="400" height="250">
+        <img src="../assets/help_invite_friend_3.jpg" alt="Tax" width="400" height="250">
+        <br>
+        <br>
 
-        <h3>Edit profile</h3>
-        <h3>Edit cover</h3>
-        <h3>Invite friend to friendship book</h3>
-        <h3>Create new friendship book page</h3>
+        <p>Click "Invite friend" to create a code for a new friendship book page. The new code will show on the left side. All codes are listed on the right side. Give the code to your friend. Your friend can enter the code on the home page and create a new friendship book page.</p>
+
+        <div style="text-align:center">
+        <img src="../assets/help_invite_friend_4.jpg" alt="Tax" width="400" height="250">
+        </div>
+        <p>All the pages can be found in your profile on the right side. </p>
+        <img src="../assets/friendshipBookCodes.jpg" alt="Tax" width="450" height="250">
+        <img src="../assets/help_invite_friend_5.jpg" alt="Tax" width="300" height="250">
+        <br>
+        <br>
+        <h4><a id="register">Register for an account at bookly</a></h4>
+        <br>
+        <p>Click register and fill in your personal data. If you are younger than 13 years, please ask your parents or an adult for permission before using this site.</p>
+        <img src="../assets/help_register.jpg" alt="register1" width="400" height="350">
+        <img src="../assets/help_register_2.jpg" alt="register2" width="400" height="350">
+
+
     </div>
 </template>
 
@@ -18,8 +48,14 @@
     export default {
         name: "help"
     }
+
+
 </script>
 
 <style scoped>
-
+    .main {
+        padding: 32px;
+        margin-left: 18%;
+        margin-right: 18%;
+    }
 </style>
